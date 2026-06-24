506fb1c8038222f92a52c84bf5bce109b943cbae
Nay <168801233+Eseiza@users.noreply.github.com>
2026-06-24 16:49:48 -0300

    Fix product array syntax and clean up code

--- app.js
+++ app.js
@@ -3,23 +3,18 @@ const productos = [
         nombre: "Envase 1L",
         codigo: "ENV001",
         stock: 150
-    },
-    {
+    },{
         nombre: "Envase 2L",
         codigo: "ENV002",
         stock: 80
-    },
-    {
+    },{
         nombre: "Tapa Negra",
         codigo: "TAP001",
         stock: 450
     }
 ];
-
 const tabla = document.getElementById("tablaStock");
-
 productos.forEach(producto => {
-
     tabla.innerHTML += `
         <tr>
             <td>${producto.nombre}</td>
@@ -27,5 +22,4 @@ productos.forEach(producto => {
             <td>${producto.stock}</td>
         </tr>
     `;
-
 });
