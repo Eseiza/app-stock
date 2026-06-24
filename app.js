639fb20c660ba05033877536314b49d3243d4c3a
Nay <168801233+Eseiza@users.noreply.github.com>
2026-06-24 15:57:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+const productos = [
+    {
+        nombre: "Envase 1L",
+        codigo: "ENV001",
+        stock: 150
+    },
+    {
+        nombre: "Envase 2L",
+        codigo: "ENV002",
+        stock: 80
+    },
+    {
+        nombre: "Tapa Negra",
+        codigo: "TAP001",
+        stock: 450
+    }
+];
 
+const tabla = document.getElementById("tablaStock");
+
+productos.forEach(producto => {
+
+    tabla.innerHTML += `
+        <tr>
+            <td>${producto.nombre}</td>
+            <td>${producto.codigo}</td>
+            <td>${producto.stock}</td>
+        </tr>
+    `;
+
+});
